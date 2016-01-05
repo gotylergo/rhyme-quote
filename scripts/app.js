bf902ee68840f9637d3812f09ceed5cb6c35895c
gotylergo <tk03759@gmail.com>
2016-01-05 14:31:08 -0500

    Random background functionality, started AJAX code

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,24 @@
 $(function() {
+
+	// Rotating background images
     var bgArray = ["bg1.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg", "bg5.jpg", "bg6.jpg", "bg8.jpg", "bg9.jpg", "bg10.jpg", "bg11.jpg", "bg12.jpg", "bg13.jpg"];
     var bg = bgArray[Math.floor(Math.random() * bgArray.length)];
     var path = 'images/';
-    $("body").css("background-image","url('"+path+bg+"')"); 
+    $("body").css("background-image","url('"+path+bg+"')");
+
+    // Get input
+    $("#getInput").submit(function(event) {
+    	event.preventDefault();
+    	var tag = $("#topic").val();
+
+    });
+    $.ajax({
+    	url: "https://favqs.com/api/",
+    	data: request,
+    	dataType: "jsonp",
+    	type: "GET",
+    })
+    var request = {
+    	filter: tag,
+    }
 });
