4f5bfc6d008a59db196e8c3bbb6157845e5526ac
gotylergo <tk03759@gmail.com>
2016-01-07 17:05:05 -0500

    Cache background images

--- scripts/app.js
+++ scripts/app.js
@@ -9,7 +9,7 @@ $(function() {
 	// Rotating background images
 
 	var changeBackground = function() {
-    var bgArray = ["bg1.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg", "bg5.jpg", "bg6.jpg", "bg8.jpg", "bg9.jpg", "bg10.jpg", "bg11.jpg", "bg12.jpg", "bg13.jpg"];
+    var bgArray = ["bg1.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg", "bg5.jpg", "bg6.jpg", "bg7.jpg","bg8.jpg", "bg9.jpg", "bg10.jpg", "bg11.jpg", "bg12.jpg", "bg13.jpg", "bg14.jpg"];
     var bg = bgArray[Math.floor(Math.random() * bgArray.length)];
     var path = 'images/';
     $(".content").stop().animate({backgroundColor:'rgba(0, 0, 0, 1)'}, 600 ,function(){
