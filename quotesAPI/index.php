e6db776c4d765a24ff2ee20efafd0a98a35322b6
gotylergo <tk03759@gmail.com>
2018-11-15 00:48:21 -0800

    Move quotes API into project

---
+++ quotesAPI/index.php
@@ -0,0 +1,42 @@
+<?php
+$con = mysql_connect('mysql.tylerjustyn.com','tjquotesdb','qtyco2846');
+mysql_select_db('tylerjustynquotes');
+
+$data = '{}'; // json string
+
+if(isset($_REQUEST['q'])){
+	$search = $_REQUEST['q'];
+	$q = mysql_query("select * from quotes where quote LIKE '% {$search}.'");
+	$results = [];
+	while ($row = mysql_fetch_object($q)) {
+		$result = [];
+		$result['quote'] = $row->quote;
+		$result['author'] = $row->author;
+		$results[] = $result;
+	}
+	$data = json_encode($results);
+}
+
+
+if(array_key_exists('callback', $_GET)){
+
+    header('Content-Type: text/javascript; charset=utf8');
+    header('Access-Control-Allow-Origin: http://www.example.com/');
+    header('Access-Control-Max-Age: 3628800');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
+    $callback = $_GET['callback'];
+    echo $callback.'('.$data.')';
+
+}else{
+    // normal JSON string
+    header('Content-Type: application/json; charset=utf8');
+
+    echo $data;
+}
+
+	
+
+
+	// 
+	
