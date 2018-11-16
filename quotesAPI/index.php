77f9357734c0baa47bf7730e21f126623d8a1f00
gotylergo <tk03759@gmail.com>
2018-11-15 21:04:27 -0800

    Fix quotesAPI and add timeouts

--- quotesAPI/index.php
+++ quotesAPI/index.php
@@ -21,7 +21,7 @@ if(isset($_REQUEST['q'])){
 if(array_key_exists('callback', $_GET)){
 
     header('Content-Type: text/javascript; charset=utf8');
-    header('Access-Control-Allow-Origin: http://www.tylerjustyn.com/');
+    header('Access-Control-Allow-Origin: *');
     header('Access-Control-Max-Age: 3628800');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
--- scripts/app.js
+++ scripts/app.js
@@ -1,46 +1,51 @@
-$(function() {
+$(function () {
 
 	// Declare variables
 	let randomQuoteNo;
 	let quote1;
 	let author1;
-	let randomRhymxeNo;
 
 	// Rotating background images
 
-	const changeBackground = function() {
-	    const bgArray = ["bg1.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg", "bg5.jpg", "bg6.jpg", "bg7.jpg", "bg8.jpg", "bg9.jpg", "bg10.jpg", "bg11.jpg", "bg12.jpg", "bg13.jpg"];
-	    let bg = bgArray[Math.floor(Math.random() * bgArray.length)];
-	    const path = "images/";
-	    $(".content").stop().animate({backgroundColor: "rgba(0, 0, 0, 1)"}, 600 ,function() {
-		    $("main").css("background-image",`url('${path}${bg}')`);
-		    $(".content").stop().animate({backgroundColor: "rgba(0, 0, 0, 0.2)"}, 600);
+	const changeBackground = function () {
+		const bgArray = ["bg1.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg", "bg5.jpg", "bg6.jpg", "bg7.jpg", "bg8.jpg", "bg9.jpg", "bg10.jpg", "bg11.jpg", "bg12.jpg", "bg13.jpg"];
+		let bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+		const path = "images/";
+		$(".content").stop().animate({ backgroundColor: "rgba(0, 0, 0, 1)" }, 600, function () {
+			$("main").css("background-image", `url('${path}${bg}')`);
+			$(".content").stop().animate({ backgroundColor: "rgba(0, 0, 0, 0.2)" }, 600);
 		});
 	};
 	changeBackground();
 
-    // Get input
+	// Get input
 
-    $("#getInput").submit(function(event) {
+	$("#getInput").submit(function (event) {
 		$(".load").show();
-    	event.preventDefault();
-    	let tag = $("#topic").val();
-    	changeBackground();
-    	// Get random quote 1
+		event.preventDefault();
+		let tag = $("#topic").val();
+		changeBackground();
+		// Get random quote 1
 
-	    $.ajax({
-	    	url: "/quotesAPI",
-	    	data: {"q" : tag},
-	    	dataType: "jsonp",
-	    	crossDomain: true,
-	    	type: "GET",
-	    	success: function(quoteList){
+		$.ajax({
+			url: "quotesAPI",
+			data: { "q": tag },
+			dataType: "jsonp",
+			crossDomain: true,
+			type: "GET",
+			error: function (jqXHR, textStatus) {
+				if (textStatus === 'timeout') {
+					alert('Failed to find a result in time. Try again?');
+					$('.load').hide();
+				}
+			},
+			success: function (quoteList) {
 				// If no quote1 found, alert user to try again	    	
-		    	if (quoteList.length === 0) {
-		    		$("#quote1").html(`No quotes found ending in "${tag}"`);
+				if (quoteList.length === 0) {
+					$("#quote1").html(`No quotes found ending in "${tag}"`);
 					$("#quote2").html("Please try again.");
 				}
-		    	// If quote1 found
+				// If quote1 found
 				else {
 					randomQuoteNo = Math.floor(Math.random() * quoteList.length);
 					quote1 = quoteList[randomQuoteNo].quote;
@@ -50,53 +55,59 @@ $(function() {
 
 					// Get rhyme
 
-				    $.ajax({
-				    	url: "//rhymebrain.com/talk",
-				    	data: {"function" : "getRhymes", "word" : tag, "score" : "300", "frequency" : 28, "maxResults" : 3},
-				    	dataType: "json",
-				    	type: "GET",
-				    })
-
-				    .done(function(rhymeWordList) {
-				    	let i = 0;
-				    	getRhymeWord(i, rhymeWordList);
+					$.ajax({
+						url: "//rhymebrain.com/talk",
+						data: { "function": "getRhymes", "word": tag, "score": "300", "frequency": 28, "maxResults": 3 },
+						dataType: "json",
+						type: "GET",
+					})
+						.done(function (rhymeWordList) {
+							let i = 0;
+							getRhymeWord(i, rhymeWordList);
+						});
+				}
+				function getRhymeWord(i, rhymeWordList) {
+					if (i < rhymeWordList.length) {
+						getSecondQuote(i, rhymeWordList);
+					} else {
+						$("#quote2").html("Cannot find a matching quote that rhymes");
+					}
+				}
+				function getSecondQuote(i, rhymeWordList) {
+					randomNo = Math.floor(Math.random() * rhymeWordList.length);
+					tag = rhymeWordList[randomNo].word;
+					rhymeWordList.splice(randomNo, 1);
+					$.ajax({
+						url: "quotesAPI",
+						data: { "q": tag },
+						dataType: "jsonp",
+						crossDomain: true,
+						type: "GET",
+						success: function (quoteList) {
+							if (quoteList.length > 0) {
+								randomQuoteNo = Math.floor(Math.random() * quoteList.length);
+								quote2 = quoteList[randomQuoteNo].quote;
+								author2 = quoteList[randomQuoteNo].author;
+								$("#quote2").html(`"${quote2}"<span> - ${author2}</span>`);
+							} else {
+								getRhymeWord(i, rhymeWordList);
+							}
+						},
+						error: function(jqXHR, textStatus) {
+							if(textStatus === 'timeout') {
+								alert('Failed to find a result in time. Try again?');
+								$('.load').hide();
+							}
+						},
+						complete: function () {
+							$('.load').hide();
+						},
+						timeout: 3000,
 					});
 				}
-					function getRhymeWord(i, rhymeWordList){
-						if(i<rhymeWordList.length){
-							getSecondQuote(i,rhymeWordList);
-						} else {
-							$("#quote2").html("Cannot find a matching quote that rhymes");
-						}
-					}
-					function getSecondQuote(i, rhymeWordList){
-						randomNo = Math.floor(Math.random() * rhymeWordList.length);
-						tag = rhymeWordList[randomNo].word;
-						rhymeWordList.splice(randomNo,1);
-						$.ajax({
-						    url: "/quotesAPI",
-						    data: {"q" : tag},
-						    dataType: "jsonp",
-						    crossDomain: true,
-						    type: "GET",
-						    success: function(quoteList){
-		    	   				if (quoteList.length > 0) {
-		    						randomQuoteNo = Math.floor(Math.random() * quoteList.length);
-		    						quote2 = quoteList[randomQuoteNo].quote;
-		    						author2 = quoteList[randomQuoteNo].author;
-		    			   			$("#quote2").html(`"${quote2}"<span> - ${author2}</span>`);
-		    			   		} else {
-		    			   			getRhymeWord(i,rhymeWordList);
-		    			   		}
-						    },
-						    complete: function() {
-						    	$('.load').hide();
-						    }
-					    });
-					
-					}
-	    	}
-	    });
+			},
+			timeout: 6000,
+		});
 	});
 	$("#modal1").modal();
 });
